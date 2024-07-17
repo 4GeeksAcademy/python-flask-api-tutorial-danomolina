c8f9de993fb4856e8f005a5dc133d5994473d2c0
Danomolina <danomolina@gmail.com>
2024-07-17 21:37:18 +0000

    PythonFlaskApi

--- src/app.py
+++ src/app.py
@@ -9,11 +9,21 @@ def hello_world():
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.json()
+    request_body = request.json
     print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    todos.append(request_body)
+    return jsonify(todos)
 
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+
+    todos.pop(position)
+
+    return jsonify(todos)
 
 # These two lines should always be at the end of your app.py file
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
+
+
